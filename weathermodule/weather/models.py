f588d7d796bb1ea560fc2b33a7c12e57592e8407
Just <msg.msgq@mail.ru>
2022-08-11 11:40:18 +0300

    Добавил отображение графиков в пределах 5 минут (за день и неделю)

--- weathermodule/weather/models.py
+++ weathermodule/weather/models.py
@@ -44,3 +44,6 @@ class Indicators(models.Model):
     tgrounddeep = models.FloatField()
     wingspeed = models.FloatField()
     wingdir = models.FloatField()
+
+    def __lt__(self, other):
+        return self.dt < other.dt
